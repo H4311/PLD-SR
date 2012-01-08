d9eacd9f0a0d119848200dc6c805b1c4098d519c
tpatel <thibaut.patel@gmail.com>
2012-01-08 15:14:50 +0100

    Ajout de la recherche concernant les données météo récoltées d'internet

---
+++ res/reseau/fluxInternet/bash/temperature.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+curl -s http://www.google.co.uk/ig/api?weather=`echo $1 | sed "s/ /%20/g"` | LANG=UTF-8 sed "s/.*temp_c data=\"\([0-9]*\).*/\1/" > temperature.today
+
+echo "La température actuelle à $1 est de `cat temperature.today` degres celcius."
