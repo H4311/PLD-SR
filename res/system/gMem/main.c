6be8b88c43b855e329fc29def63b40261dae2300
Benjamin Bouvier <bnjbouv+signup@gmail.com>
2012-01-11 22:18:46 +0100

    GMem : première implémentation

--- res/system/gMem/main.c
+++ res/system/gMem/main.c
@@ -1,6 +1,7 @@
 # include "gmem.h"
 # include "ctx.h"
 
+# include "assert.h"
 # include "stdio.h"
 
 int main(int argc, char** argv)
@@ -8,7 +9,13 @@ int main(int argc, char** argv)
 	int* table = 0;
 	
 	context = create_ctx();
-	table = (int*) gmalloc(sizeof(int)*10);	
+	
+	table = (int*) gmalloc(1023);
+	assert(table != NULL);
+	gfree(table);
+	
+	table = (int*) gmalloc(1);
+	assert(table != NULL);
 	gfree(table);
 	
 	destroy_ctx(context);
