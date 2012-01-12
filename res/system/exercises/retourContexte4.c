a010438b9d00d0f6a28b71a04a598024be9f76e6
jtuloup <jeremy.tuloup@gmail.com>
2012-01-12 23:26:15 +0100

    recherches sur sauvegarde du contexte ... pas glop

--- res/system/exercises/retourContexte4.c
+++ res/system/exercises/retourContexte4.c
@@ -11,8 +11,25 @@ typedef struct {
 } ctx_s;
 
 ctx_s* context;
+static int retValue;
 
 typedef int (func_t)(int);
+
+int doNothing()
+{
+	return 0;
+}
+
+void displayContext() 
+{
+	int bp, sp;
+	asm ("movl %%ebp, %0" "\n\t" "movl %%esp, %1"
+	: "=r"(bp), "=r"(sp) /* output variables */
+	: /* input variables */
+	);
+	
+	printf("ebp : %d, esp : %d \n", bp, sp);
+}
 
 int try(ctx_s *pctx, func_t* f, int arg)
 {	
@@ -23,26 +40,30 @@ int try(ctx_s *pctx, func_t* f, int arg)
 	: /* input variables */
 	);
 
+	printf("ebp save : %d, esp save : %d\n", pctx->ebp, pctx->esp);
+
 	if (pctx->mustReturnValue) {
 		return 1337; /*pctx->returnValue;*/
 	} else {
 		return f(arg);
 	}
+	
 }
 
-int throw(ctx_s *pctx, int r)
+int throw(ctx_s *pctx)
 {
 	pctx->mustReturnValue = 1;
-	pctx->returnValue = r;
+	pctx->returnValue = retValue;
 
 	long ebp = pctx->ebp;
 	long esp = pctx->esp;
 
-	asm ("movl %1, %%esp" "\n\t" "movl %0, %%ebp" "\n\t" "ret"
-	:  /* output variables */
+	asm ("movl %1, %%esp" "\n\t" "movl %0, %%ebp" "\n\t" /*"pop %%ebp" "\n\t" "ret"*/
+	: /* output variables */
 	: "r"(ebp), "r"(esp) /* input variables */
-	: "%esp"/*, "%ebp" */
+	: /*"%esp"/*, "%ebp" */
 	);
+
 }
 
 static int mul(int depth)
@@ -53,16 +74,23 @@ static int mul(int depth)
 		case EOF :
 			return 1; /* neutral element */
 		case 0 :
+			printf("case 0");
 			return mul(depth+1); /* erroneous read */
 		case 1 :
 			if (i)
+			{
 				return i * mul(depth+1);
-			else
-				throw(context, -1);
+			}
+			else 
+			{
+				retValue = -1;
+				throw(context);
 				return 0;
+			}
 		default:
 			return 0;
 	}
+	
 }
 
 int main()
@@ -70,8 +98,9 @@ int main()
 	int product;
 	context = (ctx_s*) malloc(sizeof(ctx_s));
 
+	retValue = 0;
 	printf("A list of int, please\n");
-	product = try(context, (func_t*) mul, 0);
+	product = try(context, (func_t*) mul, retValue);
 	printf("product = %d\n", product);
 
 	free(context);
