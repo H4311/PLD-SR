6b99c6760cae3d6bb44e8b18eeace41b68752f01
Benjamin Bouvier <bnjbouv+signup@gmail.com>
2012-01-27 11:51:19 +0100

    [Webserver] serveur nodejs

---
+++ res/webserver/server.js
@@ -0,0 +1,23 @@
+var http = require("http");
+var url = require("url");
+
+function startServer(route, handle) {
+	function onRequest(req, resp) {
+		var pathname = url.parse(req.url).pathname;
+		console.log("Request received for " + pathname);
+		req.setEncoding("utf8");
+		var postData = "";
+		req.addListener("data", function(chunk) {
+			postData += chunk;
+		});
+
+		req.addListener("end", function() {
+			content = route(handle, pathname, resp, postData);
+		});
+	}
+	
+	http.createServer(onRequest).listen(1337);
+	console.log("Webserver launched");
+}
+
+exports.start = startServer;
