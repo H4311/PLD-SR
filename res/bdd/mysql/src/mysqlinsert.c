4fa89699f26a0e7b8acf9c2d8a2f0237ab1a7ca5
tpatel <thibaut.patel@gmail.com>
2012-01-30 21:39:10 +0100

    Code fonctionel, reste la prise en compte des cas d'erreur et la gestion du timestamp

--- res/bdd/mysql/src/mysqlinsert.c
+++ res/bdd/mysql/src/mysqlinsert.c
@@ -1,17 +1,16 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
+#include <string.h>
 #include "mysqlinsert.h"
 
 MYSQL* connectToMysql() {
 	MYSQL* mysql = mysql_init(NULL);
-	if(mysql) {
-		puts("baaad");
+	if(mysql == NULL) {
+		puts("Erreur de mysql_init");
 	}
 
-	if (mysql_real_connect(mysql,"localhost","hexanome","hexanome","pld",0,NULL,0)) {
+	if (!mysql_real_connect(mysql,"localhost","hexanome","hexanome","pld",0,NULL,0)) {
 		fprintf(stderr, "Error: failed to connect to database (%s)\n", mysql_error(mysql));
-	} else {
-		puts("connecté");
 	}
 	return mysql;
 }
@@ -22,24 +21,62 @@ void closeMysql(MYSQL* mysql) {
 
 void insertCapteur(MYSQL* mysql, int type, int numeroCapteur) {
 	char s[64];
-	sprintf(s, "insert into capteurs (type, numeroCapteur) values (%d, %d)", type, numeroCapteur);
+	sprintf(s, "INSERT INTO capteurs (type, numeroCapteur) VALUES (%d, %d)", type, numeroCapteur);
 
 	if(mysql_query(mysql, s)) {
 		printf("Erreur: insert (%s)\n", mysql_error(mysql));
-	} else {
-		puts("okCapteur");
 	}
 }
 
 void insertMesure(MYSQL* mysql, int idCapteur, int time, int typeMesure, double mesure) {
-	char s[64];
-	sprintf(s, "insert into mesure (idCapteur, time, typeMesure, mesure) values (%d, %d, %d, %f)",
-		idCapteur, time, typeMesure, mesure);
+	char s[128];
+	
+	MYSQL_TIME  ts;
+	MYSQL_BIND  bind[4];
+	MYSQL_STMT  *stmt;
+	
+	stmt = mysql_stmt_init(mysql);
 
-	if(mysql_query(mysql, s)) {
-				printf("Erreur: insert (%s)\n", mysql_error(mysql));
-	} else {
-		puts("okMesure");
-	}
+	sprintf(s, "INSERT INTO mesures (idCapteur, time, typeMesure, mesure) VALUES (?,?,?,?)");
+	
+	mysql_stmt_prepare(stmt, s, strlen(s));
+	
+	memset(bind, 0, sizeof(bind));
+
+	bind[0].buffer_type= MYSQL_TYPE_LONG;
+	bind[0].buffer= (char *)&idCapteur;
+	bind[0].is_null= 0;
+	bind[0].length= 0;
+	
+	bind[1].buffer_type= MYSQL_TYPE_TIMESTAMP;
+	bind[1].buffer= (char *)&ts;
+	bind[1].is_null= 0;
+	bind[1].length= 0;
+	
+	bind[2].buffer_type= MYSQL_TYPE_LONG;
+	bind[2].buffer= (char *)&typeMesure;
+	bind[2].is_null= 0;
+	bind[2].length= 0;
+	
+	bind[3].buffer_type= MYSQL_TYPE_DOUBLE;
+	bind[3].buffer= (char *)&mesure;
+	bind[3].is_null= 0;
+	bind[3].length= 0;
+
+	mysql_stmt_bind_param(stmt, bind);
+
+	/* Fourni les données à envoyer dans la structure ts */
+	ts.year= 2002;
+	ts.month= 02;
+	ts.day= 03;
+
+	ts.hour= 10;
+	ts.minute= 45;
+	ts.second= 20;
+
+	mysql_stmt_execute(stmt);
+	
+	mysql_stmt_close(stmt);
+	
 }
 
--- res/bdd/mysql/src/test.c
+++ res/bdd/mysql/src/test.c
@@ -9,22 +9,16 @@ int main(){
 	mysql_library_init(0, NULL, NULL);
 	
 	//For each thread :
-	puts("main");
 	MYSQL* mysql = connectToMysql();
 	
-	puts("main");
-	/*insertCapteur(mysql, 1, 4);
-	puts("main");
+	insertCapteur(mysql, 1, 4);
 	insertMesure(mysql, 4, 100, 2, 5);
-	puts("main");
 	
 	closeMysql(mysql);
-	puts("main");
 	
 	//Only one time in the prog, to close the lib
 	mysql_library_end();
 
-	*/
 	return 0;
 	
 }
