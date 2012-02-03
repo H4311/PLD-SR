f70cf21952c73ce25c057871cfec24238d98f439
alahache <arnaud.lahache@gmail.com>
2012-02-03 01:09:07 +0100

    Beggining of the Node.JS Web server based on BenjBouv researches

---
+++ server/web/httpserver.js
@@ -0,0 +1,37 @@
+var http = require("http");
+var url = require("url");
+
+function route(handlers, pathname, query, postData, resp) {
+	if (typeof handlers[pathname] === 'function') {
+		console.log("Route received for : " + pathname);
+		handlers[pathname](query, postData, resp);
+	} else	{
+		console.log("No route found for : " + pathname + ". Trying static serving...");
+		serveStatic(pathname, resp);
+	}
+}
+
+function httpStart(port, handlers) {
+	function onRequest(req, resp) {
+		var urlObj = url.parse(req.url, true);	// Parse the url AND the query
+		var pathname = urlObj.pathname;
+		var query = urlObj.query;
+		
+		console.log("Request received for " + pathname);
+		req.setEncoding("utf8");
+
+		var postData = "";
+		req.addListener("data", function(chunk) {
+			postData += chunk;
+		});
+
+		req.addListener("end", function() {
+			route(handlers, pathname, query, postData, resp);
+		});
+	}
+	
+	http.createServer(onRequest).listen(port);
+	console.log("Webserver launched");
+}
+
+exports.start = httpStart;
