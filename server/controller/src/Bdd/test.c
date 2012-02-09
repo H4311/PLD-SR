f48a31aac60f411ea202a6387e6a4e2d545a4420
tpatel <thibaut.patel@gmail.com>
2012-02-09 23:17:08 +0100

    Ajout des fonctions d'insertion de base

--- server/controller/src/Bdd/test.c
+++ server/controller/src/Bdd/test.c
@@ -16,6 +16,10 @@ int main(){
 	time_t t;
 	time(&t);
 	insertMesure(mysql, 1, 4, t, 2, 5);
+	insertActionneur(mysql, 10);
+	insertActionneurSujet(mysql, "Action action !", "Ca agit..", 1, 10);
+	insertPieces(mysql, "Chambre", "On dort ici");
+	insertPatients(mysql, "Joe", 1, "Gelure des orteils", 2);
 	
 	closeMysql(mysql);
 	
