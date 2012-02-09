5605ed0e4f84cae9f4d04869966685b72ca1d21f
tpatel <thibaut.patel@gmail.com>
2012-02-09 20:41:07 +0100

    Bon ordre des tables

--- server/bdd/createTables.sql
+++ server/bdd/createTables.sql
@@ -2,23 +2,17 @@ USE pld;
 
 DROP TABLE IF EXISTS capteurs, mesures, pieces, patients, actionneurs, actionneurSujet;
 
-CREATE TABLE capteurs (
+CREATE TABLE actionneurs (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	type INTEGER,
-	numeroCapteur INTEGER,
-	isGlobal BOOLEAN,
-	idSujet INTEGER,
-	UNIQUE (type, numeroCapteur),
-	FOREIGN KEY (idPatient) REFERENCES patients (id) 
+	type INTEGER
 );
 
-CREATE TABLE mesures (
+CREATE TABLE actionneurSujet (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	idCapteur INTEGER,
-	time BIGINT,
-	typeMesure INTEGER,
-	mesure REAL,
-	FOREIGN KEY (idCapteur) REFERENCES capteurs (id)
+	nom VARCHAR(32),
+	description VARCHAR(64),
+	isGlobal BOOLEAN,
+	idSujet INTEGER
 );
 
 CREATE TABLE pieces (
@@ -36,15 +30,21 @@ CREATE TABLE patients (
 	FOREIGN KEY (idPiece) REFERENCES pieces (id)
 );
 
-CREATE TABLE actionneurs (
+CREATE TABLE capteurs (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	type INTEGER
+	type INTEGER,
+	numeroCapteur INTEGER,
+	isGlobal BOOLEAN,
+	idSujet INTEGER,
+	UNIQUE (type, numeroCapteur),
+	FOREIGN KEY (idPatient) REFERENCES patients (id) 
 );
 
-CREATE TABLE actionneurSujet (
+CREATE TABLE mesures (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	nom VARCHAR(32),
-	description VARCHAR(64),
-	isGlobal BOOLEAN,
-	idSujet INTEGER
+	idCapteur INTEGER,
+	time BIGINT,
+	typeMesure INTEGER,
+	mesure REAL,
+	FOREIGN KEY (idCapteur) REFERENCES capteurs (id)
 );
