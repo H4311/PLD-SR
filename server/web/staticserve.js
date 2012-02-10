bec52c29babd6a0185b57ef53863fb49630136cc
Benjamin Bouvier <bnjbouv+signup@gmail.com>
2012-02-10 14:19:32 +0100

    [Webserver] Including system. Add {{include filename.html}} to include it into another file.

--- server/web/staticserve.js
+++ server/web/staticserve.js
@@ -2,6 +2,44 @@ var fs = require("fs");
 var path = require("path");
 
 var htmlPath = "html";
+var includePath = "include";
+
+// callback(exists, error, content)
+/* This function loads all the subfiles included in a file, replacing
+ * one at a call (this is a recursive function). */
+function nextReplacement(content, results, i, callback) {
+	
+	// If we are at the end of the matches, we just have to stop.
+	if (i >= results.length) {
+		callback(1, 0, content);
+		return;
+	}
+	
+	// Else, retrieves the file name, includes the file and loops to 
+	// the next one.
+	var subRegex = /{{include (.*)}}/;
+	var fileName = results[i].match(subRegex)[1];
+	getStaticContent("/" + includePath + "/" + fileName, function(exists, error, subcontent) {
+		if (exists && !error) {
+			content = content.toString().replace("{{include " + fileName + "}}", subcontent);
+		} // if the file doesn't exist or there is an error on lecture,
+		  // we just skip to the next inclusion.
+		nextReplacement(content, results, i+1, callback); // recursive call
+	});
+}
+
+// callback(exists, error, content)
+/* Renders the includes into a file */
+function renderInclude(content, callback) {
+	var regex = /{{include ([a-zA-Z]*).html}}/g;
+	var results = content.toString().match(regex);
+	
+	if (results) {
+		nextReplacement(content, results, 0, callback);	
+	} else {
+		callback(1, 0, content);
+	}
+}
 
 // callback(exists, error, content)
 function getStaticContent(pathname, callback) {
@@ -22,7 +60,9 @@ function getStaticContent(pathname, callback) {
 		if (exists) {
 			console.log("File " + pathname + " exists.");
 			fs.readFile(pathname, function (error, content) {
-				callback(exists, error, content);
+				if (!error) {
+					renderInclude(content, callback);
+				}
 			});
 		} else {
 			console.log("Path " + pathname + " not found.");
