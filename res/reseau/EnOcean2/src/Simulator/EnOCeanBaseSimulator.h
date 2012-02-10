269d3d4db3d7af39dc01a91073716990dfb59e58
Benjamin Planche <bill.murmure@gmail.com>
2012-02-10 11:49:26 +0100

    <EnOcean> Debug API Commande + Sauvetage code BDD + Simulation Sunspot

--- res/reseau/EnOcean2/src/Simulator/EnOCeanBaseSimulator.h
+++ res/reseau/EnOcean2/src/Simulator/EnOCeanBaseSimulator.h
@@ -39,7 +39,7 @@ class EnOCeanBaseSimulator
 
 public:
 //------------------------------------------------------- Public Constants
-	static const int DELAY = 5;
+	static const int DELAY = 30;
 //--------------------------------------------------------- Public Methods
 
 	void addSensor(SensorSimulator* sensor);
