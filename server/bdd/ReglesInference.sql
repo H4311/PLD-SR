ea3b10fca4ddc2e54f02bfb525e9d9202b3cc88e
tpatel <thibaut.patel@gmail.com>
2012-02-17 08:43:39 +0100

    Suppression event dans regleinference.sql

--- server/bdd/ReglesInference.sql
+++ server/bdd/ReglesInference.sql
@@ -1,7 +1,5 @@
 
 INSERT INTO actionneurs (numeroActionneur, type) VALUES (1337, 42);
-INSERT INTO events (id, idActionneur, valeur) VALUES (5, 1337, 16);
-INSERT INTO events (id, idActionneur, valeur) VALUES (10, 1337, 64);
 
 INSERT INTO regles (id, nom, createsAlert) VALUES (100, 'chauffageON', true);
 INSERT INTO regles (id, nom, createsAlert) VALUES (101, 'chauffageOFF', true);
