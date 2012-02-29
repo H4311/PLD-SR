38c77246a798a5e3411f69652f6c8bb16b97d523
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2012-02-29 18:34:59 +0100

    Ajout d'indications pour le script de setup de la BDD

--- server/bdd/setUpDb.sh
+++ server/bdd/setUpDb.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Création de la bdd. Mot de passe root requis."
 mysql -u root -p < creationDB.sql
+echo "Création de l'utilisateur. Mot de passe root requis."
 mysql -u root -p < createUser.sql
+echo "Création des tables. Mot de passe rithm requis."
 mysql -u rithm -p pld < createTables.sql
+echo "Insertion des valeurs dans les tables. Mot de passe rithm requis."
 mysql -u rithm -p pld < insertValues.sql
