38fcdbe0ea5c19fe34e9383799009da276595fb6
Arnaud Lahache <arnaud.lahache@gmail.com>
2012-03-02 00:56:57 +0100

    Démarrage et arrêt du serveur

--- server/stopServer.sh
+++ server/stopServer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Suppression des serveurs et ssh..."
 for line in $(cat pids); do
     kill "$line"
 done
+echo "Arrêt de MySQL..."
 sudo service mysql stop
+echo "Suppression du fichier de PIDs..."
 rm pids
+echo "Everything is OK."
